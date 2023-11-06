396c0a9446144f95205c3ec312ffb077d3667ca6
akinseman <145451296+akinseman@users.noreply.github.com>
2023-11-06 11:30:35 +0300

    Add files via upload

---
+++ hesplama/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace hesplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnhepla = new System.Windows.Forms.Button();
+            this.lbltltutar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Location = new System.Drawing.Point(137, 9);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.Size = new System.Drawing.Size(100, 20);
+            this.txtDolar.TabIndex = 0;
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.Location = new System.Drawing.Point(126, 43);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.Size = new System.Drawing.Size(100, 20);
+            this.txtMiktar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1Abddolar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(26, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Miktar";
+            // 
+            // btnhepla
+            // 
+            this.btnhepla.Location = new System.Drawing.Point(29, 96);
+            this.btnhepla.Name = "btnhepla";
+            this.btnhepla.Size = new System.Drawing.Size(179, 39);
+            this.btnhepla.TabIndex = 4;
+            this.btnhepla.Text = "Hepla";
+            this.btnhepla.UseVisualStyleBackColor = true;
+            this.btnhepla.Click += new System.EventHandler(this.btnhepla_Click);
+            // 
+            // lbltltutar
+            // 
+            this.lbltltutar.AutoSize = true;
+            this.lbltltutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbltltutar.Location = new System.Drawing.Point(26, 152);
+            this.lbltltutar.Name = "lbltltutar";
+            this.lbltltutar.Size = new System.Drawing.Size(74, 24);
+            this.lbltltutar.TabIndex = 5;
+            this.lbltltutar.Text = "Tl Tutar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 186);
+            this.Controls.Add(this.lbltltutar);
+            this.Controls.Add(this.btnhepla);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMiktar);
+            this.Controls.Add(this.txtDolar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnhepla;
+        private System.Windows.Forms.Label lbltltutar;
+    }
+}
+
---
+++ hesplama/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hesplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhepla_Click(object sender, EventArgs e)
+        {
+            double dolar = Convert.ToDouble(txtDolar.Text);
+            double miktar = Convert.ToDouble(txtMiktar.Text);   
+
+            double sonuc = dolar * miktar;
+            lbltltutar.Text = sonuc.ToString();
+        }
+    }
+}
